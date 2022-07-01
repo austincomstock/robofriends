35d1034d93953a45e71574598f77310a8e39bb06
austincomstock <austinjcomstock@gmail.com>
2022-07-01 11:19:07 -0600

    Removed unused comment

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-// import App from "./App.js";
 import App from "./containers/App.js";
 import reportWebVitals from "./reportWebVitals";
 import "tachyons";
