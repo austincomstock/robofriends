e16599ddbe5bc7f73294bca2c532068dfdf542b4
austincomstock <austinjcomstock@gmail.com>
2022-09-02 14:10:26 -0600

    Updated img alt text

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,7 +4,7 @@ const Card = ({ name, email, id }) => {
   //   const { name, email, id } = props; // see props above on how we destructred this.
   return (
     <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-      <img alt="robots" src={`https://robohash.org/${id}?200x200`} />
+      <img alt="fun robot image" src={`https://robohash.org/${id}?200x200`} />
       <div>
         <h2>{name}</h2>
         <p>{email}</p>
